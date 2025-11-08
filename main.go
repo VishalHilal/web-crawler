663f132568936fab36d7f3af876ab04101e2b742
VishalHilal <vishalschool24@gmail.com>
2025-11-08 21:18:39 +0530

    data extracting into json format added

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly/v2"
 )
 
+type PageData struct {
+	URL         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
 func main() {
+	var results []PageData
+
 	c := colly.NewCollector(
-		colly.AllowedDomains("golang.org", "go.dev"),
+		colly.AllowedDomains("golang.org", "go.dev"), // Only these domains
+		colly.MaxDepth(2), // Crawl depth
 	)
 
 	c.Limit(&colly.LimitRule{
@@ -24,16 +35,18 @@ func main() {
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
-		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		log.Println("Request failed:", r.Request.URL, err)
 	})
 
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		fmt.Println("Page Title:", e.Text)
-	})
+	c.OnHTML("head", func(e *colly.HTMLElement) {
+		page := PageData{
+			URL: e.Request.URL.String(),
+		}
 
-	c.OnHTML("meta[name=description]", func(e *colly.HTMLElement) {
-		desc := e.Attr("content")
-		fmt.Println("Meta Description:", desc)
+		page.Title = e.DOM.Find("title").Text()
+		page.Description, _ = e.DOM.Find(`meta[name="description"]`).Attr("content")
+
+		results = append(results, page)
 	})
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
@@ -43,10 +56,26 @@ func main() {
 		}
 	})
 
-	err := c.Visit("https://golang.org")
+	startURL := "https://golang.org"
+	fmt.Println("Starting crawl on:", startURL)
+	err := c.Visit(startURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	c.Wait()
+
+	file, err := os.Create("results.json")
+	if err != nil {
+		log.Fatal("Could not create file:", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(results); err != nil {
+		log.Fatal("Error writing JSON:", err)
+	}
+
+	fmt.Println("\n Crawl finished! Data saved to results.json")
 }
